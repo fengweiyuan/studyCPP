50003ab4f9fcf0a55ba435472262a3eb08254cfb
fengweiyuan <107639177@qq.com>
2018-12-15 23:02:03 +0800

    commit

---
+++ circle.h
@@ -0,0 +1,14 @@
+#ifndef STUDYCPP_CIRCLE_H
+#define STUDYCPP_CIRCLE_H
+//#include <iostream>
+class Circle {
+private:
+    double r;          // 半径
+public:
+    Circle();          // 构造函数
+    Circle(double R);  // 构造函数
+    double Area();     // 求面积函数
+};
+#endif
+
+
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "circle.h"
+using namespace std;
+
+int main()
+{
+    Circle c;
+    cout<<"Area="<<c.Area()<<endl;
+    return 0;
+}
